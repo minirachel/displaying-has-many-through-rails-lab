4b53d3a510646429c0278bc13cb291799beb87c3
minirachel <rachel.a.levinson@gmail.com>
2021-09-24 11:30:03 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,12 @@ ActiveRecord::Schema.define(version: 20210924144624) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
+    t.integer  "doctor_id"
+    t.integer  "patient_id"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
